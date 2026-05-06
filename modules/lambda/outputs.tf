1c59dc7f4b046da8916d02ed0e43a9193b02b11a
ncb-yji <clanic.otl@gmail.com>
2026-05-06 14:17:06 +0900

    fix: merge충돌해결

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -20,7 +20,7 @@ output "sns_notify_function_name" {
 
 output "function_names" {
   description = "Lambda 함수명 리스트 — CloudWatch 모니터링에서 Log Group/Alarm 일괄 처리에 사용. 함수 이름은 네이밍 컨벤션으로 결정되므로 deploy_lambda = false (외부 배포 등)일 때도 안정적으로 같은 값을 반환."
-  value = local.deploy_lambda ? [
+  value = var.deploy_lambda ? [
     aws_lambda_function.image_resize[0].function_name,
     aws_lambda_function.sns_notify[0].function_name,
     ] : [
